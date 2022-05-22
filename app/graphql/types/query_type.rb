617df20ed0ceeee32d9b64802ed3af059463fded
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air.local>
2022-05-22 14:48:08 -0400

    add query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,12 +6,13 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :message, MessageType, "Find a message by ID" do
+      argument :id, ID
+    end
+  
+    # Then provide an implementation:
+    def message(id:)
+      Message.find(id)
     end
   end
 end
