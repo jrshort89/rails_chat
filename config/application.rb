103274dbad4e07175f231f1a523e947a04ccd1c2
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air.local>
2022-07-03 17:22:56 -0400

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ChatApp
     # config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
